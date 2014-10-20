eb44f4d537b2f660048c2d64327f3708cf2e7212
neggwada <ngwz@github.com>
2014-10-20 08:48:06 +0200

    Atelier OpenStack - eNovance

---
+++ Atelier Openstack - eNovance Rh.mm
@@ -0,0 +1,479 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Atelier Openstack - eNovance Rh" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1413444884753"><hook NAME="MapStyle" zoom="1.001">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<node TEXT="CMP" POSITION="right" ID="ID_704340735" CREATED="1413444886127" MODIFIED="1413444888336">
+<edge COLOR="#ff0000"/>
+<node TEXT="CLoud Forms" ID="ID_803643450" CREATED="1413444892571" MODIFIED="1413444898290"/>
+</node>
+<node TEXT="Arch" POSITION="right" ID="ID_1067892162" CREATED="1413445017776" MODIFIED="1413445019192">
+<edge COLOR="#0000ff"/>
+<node TEXT="Horizon ==" ID="ID_1553373291" CREATED="1413445069314" MODIFIED="1413446346378"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      DAshboard
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Nova" ID="ID_896634714" CREATED="1413445022162" MODIFIED="1413446498461"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Compute
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Region" ID="ID_814987096" CREATED="1413446559968" MODIFIED="1413446565123">
+<node TEXT="cell" ID="ID_639988195" CREATED="1413446566593" MODIFIED="1413446572504">
+<node TEXT="Host aggregate" ID="ID_1265843196" CREATED="1413446574369" MODIFIED="1413446586996">
+<node TEXT="compute" ID="ID_542913330" CREATED="1413446621631" MODIFIED="1413446624566"/>
+<node TEXT="compute" ID="ID_590362674" CREATED="1413446621631" MODIFIED="1413446624566"/>
+<node TEXT="compute" ID="ID_1694252985" CREATED="1413446621631" MODIFIED="1413446624566"/>
+</node>
+<node TEXT="Host aggregate" ID="ID_468954511" CREATED="1413446574369" MODIFIED="1413446586996">
+<node TEXT="compute" ID="ID_1680291651" CREATED="1413446621631" MODIFIED="1413446624566"/>
+<node TEXT="compute" ID="ID_1737010790" CREATED="1413446621631" MODIFIED="1413446624566"/>
+<node TEXT="compute" ID="ID_1683666270" CREATED="1413446621631" MODIFIED="1413446624566"/>
+</node>
+</node>
+<node TEXT="cell" ID="ID_1875243199" CREATED="1413446566593" MODIFIED="1413446572504">
+<node TEXT="Host aggregate" ID="ID_516138648" CREATED="1413446574369" MODIFIED="1413446586996">
+<node TEXT="compute" ID="ID_373884076" CREATED="1413446621631" MODIFIED="1413446624566"/>
+<node TEXT="compute" ID="ID_564951665" CREATED="1413446621631" MODIFIED="1413446624566"/>
+<node TEXT="compute" ID="ID_1264127695" CREATED="1413446621631" MODIFIED="1413446624566"/>
+</node>
+<node TEXT="Host aggregate" ID="ID_1466134903" CREATED="1413446574369" MODIFIED="1413446586996">
+<node TEXT="compute" ID="ID_1917433957" CREATED="1413446621631" MODIFIED="1413446624566"/>
+<node TEXT="compute" ID="ID_1054301562" CREATED="1413446621631" MODIFIED="1413446624566"/>
+<node TEXT="compute" ID="ID_611873139" CREATED="1413446621631" MODIFIED="1413446624566"/>
+</node>
+<node TEXT="" ID="ID_876813599" CREATED="1413446711014" MODIFIED="1413446711014"/>
+</node>
+<node TEXT="Api Cell" ID="ID_555257570" CREATED="1413446662206" MODIFIED="1413446668946">
+<node TEXT="Nova ApI" ID="ID_1490322248" CREATED="1413446670315" MODIFIED="1413446678267"/>
+</node>
+<node TEXT="Controller" ID="ID_569371568" CREATED="1413446697310" MODIFIED="1413446701472"/>
+</node>
+</node>
+<node TEXT="Glance" ID="ID_986809430" CREATED="1413445025933" MODIFIED="1413446882949">
+<hook NAME="AlwaysUnfoldedNode"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Image Storage
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="OS Nu + Gestion de configuration (Puppet&#x2026;)" ID="ID_732683030" CREATED="1413446884450" MODIFIED="1413446916828"/>
+<node TEXT="SnapShot" ID="ID_776059995" CREATED="1413446917568" MODIFIED="1413446926555"/>
+</node>
+<node TEXT="Swift" ID="ID_472228861" CREATED="1413445028322" MODIFIED="1413447230032"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Stockage
+    </p>
+  </body>
+</html>
+</richcontent>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Stockage Bloc
+    </p>
+    <p>
+      Scalable &#224; l'infini
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Consistency Server" ID="ID_1464460715" CREATED="1413447231851" MODIFIED="1413447238589">
+<node TEXT="Storage Server" ID="ID_1234371312" CREATED="1413447240229" MODIFIED="1413447243812"/>
+<node TEXT="Storage Server" ID="ID_310293696" CREATED="1413447240229" MODIFIED="1413447243812"/>
+</node>
+<node TEXT="RH Inktank Ceph" ID="ID_1127686435" CREATED="1413447336674" MODIFIED="1413447345043"/>
+<node TEXT="Glusterfs" ID="ID_1171893589" CREATED="1413447345307" MODIFIED="1413447348187"/>
+</node>
+<node TEXT="Neutron" ID="ID_231562434" CREATED="1413445031156" MODIFIED="1413446247715"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Network
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Nova Newtork" ID="ID_1102925706" CREATED="1413447437155" MODIFIED="1413447490313"/>
+<node TEXT="Open vSwitch" ID="ID_1331901470" CREATED="1413447490896" MODIFIED="1413447500383"/>
+</node>
+<node TEXT="Cinder" ID="ID_321360902" CREATED="1413445034213" MODIFIED="1413447096730"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Stockage
+    </p>
+  </body>
+</html>
+</richcontent>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mode Block
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      par ce que sinon stockage &#233;ph&#233;m&#232;re
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      iSCSI -&gt; Cinder
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Heat" ID="ID_1571773691" CREATED="1413445035661" MODIFIED="1413446228396"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Orchestration
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Ceilometer" ID="ID_317752357" CREATED="1413445039169" MODIFIED="1413447428299">
+<hook NAME="AlwaysUnfoldedNode"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Telemetry
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="BAnde passante" ID="ID_739153030" CREATED="1413447453582" MODIFIED="1413447456511"/>
+<node TEXT="I/O" ID="ID_1697337065" CREATED="1413447456759" MODIFIED="1413447459584"/>
+<node TEXT="peut etre li&#xe9;e &#xe0; l &apos;outil d&apos;orchestration" ID="ID_236963462" CREATED="1413447462029" MODIFIED="1413447476509"/>
+</node>
+<node TEXT="" ID="ID_1262115654" CREATED="1413445046008" MODIFIED="1413445046008"/>
+<node TEXT="Keystone ||" ID="ID_1770118188" CREATED="1413445050419" MODIFIED="1413446477742"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Identity manager
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="" ID="ID_1660160295" CREATED="1413445056980" MODIFIED="1413445056980"/>
+<node TEXT="RH ==" ID="ID_1091605485" CREATED="1413445061253" MODIFIED="1413445096148"/>
+</node>
+<node TEXT="+" POSITION="right" ID="ID_229111503" CREATED="1413445180155" MODIFIED="1413445182357">
+<edge COLOR="#00ff00"/>
+<node TEXT="r&#xe9;sillience applicative" ID="ID_812044814" CREATED="1413445184437" MODIFIED="1413445193619">
+<node TEXT="n&#xe9;cessite de r&#xe9;&#xe9;crire (revoir) les applications" ID="ID_1190222838" CREATED="1413445212290" MODIFIED="1413445229208"/>
+</node>
+</node>
+<node TEXT="Version IceHOuse" POSITION="right" ID="ID_1968190199" CREATED="1413445420430" MODIFIED="1413445425328">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Certification" ID="ID_626974686" CREATED="1413445426066" MODIFIED="1413445430572">
+<node TEXT="mat&#xe9;rielle" ID="ID_1644929689" CREATED="1413445466809" MODIFIED="1413445472641"/>
+<node TEXT="Hyperviseur" ID="ID_404524788" CREATED="1413445472894" MODIFIED="1413445481217"/>
+<node TEXT="OS" ID="ID_1593742757" CREATED="1413445483862" MODIFIED="1413445487686"/>
+</node>
+<node TEXT="Support 36 mois ( au lieu de 6)" ID="ID_1701547885" CREATED="1413445431048" MODIFIED="1413445450128"/>
+</node>
+<node TEXT="Projets incub&#xe9;s" POSITION="right" ID="ID_1797925625" CREATED="1413447599299" MODIFIED="1413447761788">
+<edge COLOR="#007c7c"/>
+<node TEXT="Trove" ID="ID_673831531" CREATED="1413447646201" MODIFIED="1413447667435"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Database-as-a-service
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Sahara (Ex Savana)" ID="ID_781715694" CREATED="1413447606407" MODIFIED="1413447693498">
+<hook NAME="AlwaysUnfoldedNode"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Analytics-as-a-service
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Marconi" ID="ID_1705010958" CREATED="1413447610705" MODIFIED="1413447710791"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      MessageQueue-as-a-service
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Ironic" ID="ID_1032273499" CREATED="1413447613325" MODIFIED="1413447732556"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Baremetal Provisioning/Management
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="TripleO" ID="ID_150710778" CREATED="1413447615880" MODIFIED="1413447643098"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Deployment
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Workshop" POSITION="right" ID="ID_1516876721" CREATED="1413451691959" MODIFIED="1413451695825">
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="Stackalytics.com" POSITION="right" ID="ID_1238620476" CREATED="1413454429095" MODIFIED="1413454444277">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="?" POSITION="left" ID="ID_876719052" CREATED="1413445588253" MODIFIED="1413445591527">
+<edge COLOR="#00ffff"/>
+<node TEXT="EVB &amp; DCB" ID="ID_1022624379" CREATED="1413445594066" MODIFIED="1413445606277"/>
+<node TEXT="Avx2" ID="ID_1874071677" CREATED="1413445606735" MODIFIED="1413445614038"/>
+<node TEXT="TXT &amp; Trusted Boot" ID="ID_780167588" CREATED="1413445615929" MODIFIED="1413445629138">
+<node TEXT="OAT" ID="ID_1560271645" CREATED="1413445728928" MODIFIED="1413445761519"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Open Attestation Server
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Intel SINIT ACM" ID="ID_222462080" CREATED="1413449808634" MODIFIED="1413449821062"/>
+<node TEXT="Open Attestation" ID="ID_1596647468" CREATED="1413449854172" MODIFIED="1413449862672"/>
+<node TEXT="PCR" ID="ID_301155998" CREATED="1413449865076" MODIFIED="1413449867592"/>
+<node TEXT="TPM" ID="ID_1195953338" CREATED="1413449868126" MODIFIED="1413449869676"/>
+<node TEXT="Intel NUC" ID="ID_1817158569" CREATED="1413449978454" MODIFIED="1413449982862"/>
+</node>
+<node TEXT="Virtio-SCSI" ID="ID_1204277768" CREATED="1413445646979" MODIFIED="1413445652301"/>
+<node TEXT="Virtio-Blk" ID="ID_1961081473" CREATED="1413445657579" MODIFIED="1413445663186"/>
+<node TEXT="GLusterFS" ID="ID_1182292203" CREATED="1413445674756" MODIFIED="1413445678410"/>
+<node TEXT="Intel Cloud builder" ID="ID_1317746385" CREATED="1413445687852" MODIFIED="1413445699667"/>
+<node TEXT="AMQP Message BUs" ID="ID_1612642085" CREATED="1413445850721" MODIFIED="1413446060632"/>
+<node TEXT="BDD Galera" ID="ID_872224757" CREATED="1413447828471" MODIFIED="1413447831845"/>
+<node TEXT="eDeploy" ID="ID_691606001" CREATED="1413449456237" MODIFIED="1413449525716"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      eNovance
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="SoftLay-R" ID="ID_891396902" CREATED="1413450006893" MODIFIED="1413450015384"/>
+</node>
+<node TEXT="Pr&#xe9;sentation" POSITION="left" ID="ID_61593135" CREATED="1413445801845" MODIFIED="1413445806676">
+<edge COLOR="#007c00"/>
+<node TEXT="Erwan" ID="ID_1859546295" CREATED="1413445780014" MODIFIED="1413448124446"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      RH
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Stanislas Odinot" ID="ID_1864001875" CREATED="1413445785693" MODIFIED="1413449626701"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Intel
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="R&#xe9;mi" ID="ID_446228149" CREATED="1413445827374" MODIFIED="1413445829393"/>
+<node TEXT="William" ID="ID_483659565" CREATED="1413448102551" MODIFIED="1413448116910"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      eNovance
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="" POSITION="left" ID="ID_1113892293" CREATED="1413446755569" MODIFIED="1413446788524">
+<icon BUILTIN="help"/>
+<edge COLOR="#7c007c"/>
+<node TEXT="Netflix" ID="ID_174092342" CREATED="1413446792026" MODIFIED="1413446802210">
+<node TEXT="" ID="ID_673297080" CREATED="1413446802967" MODIFIED="1413446802967"/>
+</node>
+</node>
+</node>
+</map>
